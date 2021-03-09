e2bec196e0910d9d37a98038490fa7df969be42e
ericanderson333 <ericsanderson333@gmail.com>
2021-03-08 21:37:58 -0800

    Reset Commit

--- src/com/chocan/Accounts/Member.java
+++ src/com/chocan/Accounts/Member.java
@@ -9,4 +9,5 @@ public class Member extends Account{
     public Member(String name, int number, String address, String city, String state, int zip, List<Product> productServices) {
         super(name, number, address, city, state, zip, productServices);
     }
+
 }
--- src/com/chocan/Main.java
+++ src/com/chocan/Main.java
@@ -3,9 +3,6 @@ package com.chocan;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        int cookie = 0;
-        printf("Hello World");
-        int cesar = 1;
+        //Should be Empty
     }
 }
