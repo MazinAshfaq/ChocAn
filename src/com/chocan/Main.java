9803eed2e81aa7c35fc6b08fccd5a0a18ed54141
Andy <andy.truong928@gmail.com>
2021-03-09 19:03:55 -0800

    added ProductTest

--- src/com/chocan/Main.java
+++ src/com/chocan/Main.java
@@ -1,11 +1,16 @@
 package com.chocan;
 
+import com.chocan.ProductService.Product;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         int cookie = 0;
-        printf("Hello World");
+      // printf("Hello World");
         int cesar = 1;
+
+
+
     }
 }
